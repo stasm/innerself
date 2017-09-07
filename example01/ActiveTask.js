c66a7e4897477ee2e2bb116fe00cf33dce35d8dd
Staś Małolepszy <stas@duzodobrze.pl>
2017-09-07 10:23:18 +0200

    Remove index from ActiveTask

--- example01/ActiveTask.js
+++ example01/ActiveTask.js
@@ -3,7 +3,7 @@ import html from "../index";
 export default function ActiveTask(text, index) {
     return html`
         <li>
-            ${text} ${index}
+            ${text}
             <button
                 onclick="dispatch('COMPLETE_TASK', ${index})">
                 Mark As Done</button>
